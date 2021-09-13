05c85b7636e93e6a279da1b10d2acf0b215a7c89
Dong Gyu Kim <fwics88@gmail.com>
2021-09-13 19:45:50 +0900

    no message

--- Assignment1/src/academy/pocu/comp2500/assignment1/app/Program.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/app/Program.java
@@ -1,48 +1,7 @@
 package academy.pocu.comp2500.assignment1.app;
 
-import academy.pocu.comp2500.assignment1.Blog;
-import academy.pocu.comp2500.assignment1.EPostReaction;
-import academy.pocu.comp2500.assignment1.EPostSorting;
-import academy.pocu.comp2500.assignment1.Post;
-import academy.pocu.comp2500.assignment1.PostComment;
-import academy.pocu.comp2500.assignment1.User;
-
 public class Program {
 
     public static void main(String[] args) {
-        Blog blog = new Blog("blog 1");
-
-        User author1 = new User("author1");
-        User author2 = new User("author2");
-        User read1 = new User("read1");
-        User read2 = new User("read2");
-
-        Post newPost = blog.addPost(author1, "tt11", "bb11");
-        newPost.setTag("tag11");
-        PostComment newComment = newPost.addComment(read1, "comment11");
-        newComment.addSubcomment(read2, "subcom111");
-
-        newPost = blog.addPost(author2, "tt22", "bb22");
-        newPost.setTag("tag22");
-        newComment = newPost.addComment(read1, "comment21");
-        newComment.addSubcomment(read2, "subcom211");
-        newComment.addSubcomment(read2, "subcom212");
-        newComment = newPost.addComment(read1, "comment22");
-        newComment.addSubcomment(read2, "subcom221");
-        newPost.addReaction(read1, EPostReaction.GREAT);
-        newPost.addReaction(read2, EPostReaction.FUN);
-
-
-        read1.visitBlog(blog);
-        newPost.deleteReaction(read1);
-
-        System.out.println("\n\n------------- Filter ---------------\n");
-//        read1.setAuthorFilterName(author1.getName());
-//        read1.setTagFilter("tag11");
-        read1.setPostSorting(EPostSorting.TITLE_ASCENGIND);
-
-
-        read1.visitBlog(blog);
-
     }
 }
