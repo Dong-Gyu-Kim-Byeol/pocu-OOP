13a01ca16dd587db476eacdec5fd9665e8c2a60b
Dong Gyu Kim <fwics88@gmail.com>
2021-10-04 18:48:38 +0900

    ass2

--- Assignment2/src/academy/pocu/comp2500/assignment2/ShoppingCart.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/ShoppingCart.java
@@ -3,38 +3,29 @@ package academy.pocu.comp2500.assignment2;
 import java.util.HashMap;
 
 public class ShoppingCart {
-    private final HashMap<String, Shipment> products;
+    private final HashMap<String, Product> products;
 
     // public
     public ShoppingCart() {
-        this.products = new HashMap<String, Shipment>();
+        this.products = new HashMap<String, Product>();
     }
 
-    public HashMap<String, Shipment> getProducts() {
+    public HashMap<String, Product> getProducts() {
         return products;
     }
 
     public void addProduct(final Product product) {
-        if (this.products.containsKey(product.getProductId())) {
-            this.products.get(product.getProductId()).increaseCount();
-        } else {
-            this.products.put(product.getProductId(), new Shipment(product));
-        }
+        this.products.put(product.getProductId(), product);
     }
 
     public void removeProduct(final String productId) {
-        if (this.products.containsKey(productId)) {
-            this.products.get(productId).decreaseCount();
-            if (this.products.get(productId).getCount() <= 0) {
-                this.products.remove(productId);
-            }
-        }
+        this.products.remove(productId);
     }
 
     public int getTotalPrice() {
         int totalPrice = 0;
-        for (final Shipment shipment : this.products.values()) {
-            totalPrice += shipment.getProduct().getPrice() * shipment.getCount();
+        for (final Product product : this.products.values()) {
+            totalPrice += product.getPrice();
         }
 
         return totalPrice;
