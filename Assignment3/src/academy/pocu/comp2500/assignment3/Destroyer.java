1dbd5cc3448f95d422ec19df58cd2e5f8ecead52
Desk\Desk <fwics88@gmail.com>
2021-11-13 18:29:35 +0900

    no message

--- Assignment3/src/academy/pocu/comp2500/assignment3/Destroyer.java
+++ Assignment3/src/academy/pocu/comp2500/assignment3/Destroyer.java
@@ -20,7 +20,7 @@ public final class Destroyer extends Unit {
         // 이 유닛은 알려진 것이 별로 없습니다. 이 유닛을 개발한 군사 과학자에 따르면 이 유닛은 망령을 제외한 모든 유닛을 한 프레임 만에 모두 파괴할 수 있다고 합니다.
         // (망령을 죽이지 못하는 이유는 방어막 때문)
 
-        return new AttackIntent(this, false, ImmutableIntVector2D.ZERO);
+        return new AttackIntent(this, false, ImmutableIntVector2D.ONE);
     }
 
     // 시그내처 불변
